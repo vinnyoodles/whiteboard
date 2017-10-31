4406d64f12bd81a50df2214ff55ac47bc7b29e1b
Vincent Le <vinnyoodles@gmail.com>
2017-10-31 13:44:51 -0400

    Add server code

---
+++ server/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var http = require('http')
+var socketio = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var websocket = socketio(server);
+server.listen(3000, () => console.log('listening on *:3000'));
+
+// Mapping objects to easily map sockets and users.
+var clients = {};
+var users = {};
+
+// This represents a unique chatroom.
+// For this example purpose, there is only one chatroom;
+var chatId = 1;
+
+websocket.on('connection', (socket) => {
+    clients[socket.id] = socket;
+    socket.on('user-joined', (userId) => onUserJoined(userId, socket));
+});
+
